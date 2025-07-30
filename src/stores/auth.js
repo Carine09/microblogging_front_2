addca1759a610104b604645e92fbe9b122949814
Carjardying <carolinejardy.pro@gmail.com>
2025-07-30 13:28:30 +0200

    Update: login keep the userId so we can create posts

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,19 +1,30 @@
 import { defineStore } from 'pinia';
 
 export const useAuthStore = defineStore('auth', {
-    state: () => ({
-        token: null,
-    }),
+      state: () => ({
+    token: null,
+    userId: null,
+  }),
 
-    getters: {
-        isAuthenticated: (state) => state.token !== null,
+  getters: {
+    isAuthenticated: (state) => state.token !== null,
+  },
+
+  actions: {
+    setToken(token, userId = null) {
+      this.token = token;
+      this.userId = userId;
+
+      if (token) {
+        localStorage.setItem('token', token);
+        localStorage.setItem('userId', userId);
+      } else {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userId');
+      }
     },
-
-    actions: {
-        setToken(token) {
-            this.token = token
-        },
-        logout() {
+    
+    logout() {
             fetch("http://localhost:8000/api/logout", {
                 method: 'Post',
                 headers: { Authorization: `Bearer ${this.token}` },
